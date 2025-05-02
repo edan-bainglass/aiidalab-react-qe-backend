ac988b47ac4d7938099a33c2f2ba9aedab2f9508
Edan Bainglass <edan.bainglass@gmail.com>
2025-05-02 08:14:46 +0200

    Implement pseudopotential settings

---
+++ src/aiidalab_react_qe_backend/models/input/advanced/pseudos.py
@@ -0,0 +1,110 @@
+import typing as t
+
+import pydantic as pdt
+
+from aiidalab_react_qe_backend.common.utils import (
+    CustomBaseModel,
+    Patch,
+    WithItemFormat,
+    WithLabels,
+    WithUI,
+    WithWidget,
+    if_,
+)
+
+
+class PseudopotentialSettings(CustomBaseModel):
+    model_config = pdt.ConfigDict(title="Pseudopotentials")
+
+    functional: t.Annotated[
+        t.Literal[
+            "pbe",
+            "pbe_sol",
+        ],
+        pdt.Field(
+            title="Functional",
+        ),
+        WithWidget("toggleGroup"),
+        WithLabels(
+            labels=[
+                "PBE",
+                "PBEsol",
+            ]
+        ),
+    ] = "pbe_sol"
+    family: t.Annotated[
+        t.Optional[str],
+        pdt.Field(
+            title="Family",
+        ),
+        WithWidget("toggleGroup"),
+        WithLabels(
+            labels=[
+                "PseudoDojo",
+                "SSSP",
+            ]
+        ),
+    ] = None
+    accuracy: t.Annotated[
+        t.Optional[str],
+        pdt.Field(
+            title="Accuracy",
+        ),
+        WithWidget("toggleGroup"),
+    ] = None
+    pseudopotentials: t.Annotated[
+        list[str],
+        pdt.Field(
+            title="Pseudopotentials",
+        ),
+        WithItemFormat("data-url"),
+        WithUI(
+            schema={
+                "ui:options": {"classNames": "mt-2"},
+                "items": {
+                    "ui:hideError": True,
+                    "ui:options": {"accept": ".UPF"},
+                    "generatedFrom": "structure.species",
+                    "template": "{{species}}",
+                },
+            }
+        ),
+    ]
+
+    __dependencies__ = ["basic.spin_orbit"]
+
+    __conditionals__ = [
+        if_("spin_orbit")
+        .is_false()
+        .then_(
+            patches=[
+                Patch("family").set_options(["PseudoDojo", "SSSP"]).set_default("SSSP")
+            ],
+            conditions=[
+                if_("family")
+                .equals("SSSP")
+                .then_(
+                    patches=[
+                        Patch("accuracy")
+                        .set_options(["efficiency", "precision"])
+                        .set_default("efficiency")
+                    ]
+                )
+                .else_(
+                    patches=[
+                        Patch("accuracy")
+                        .set_options(["standard", "stringent"])
+                        .set_default("standard")
+                    ]
+                )
+            ],
+        )
+        .else_(
+            patches=[
+                Patch("family").set_options(["PseudoDojo"]).set_default("PseudoDojo"),
+                Patch("accuracy")
+                .set_options(["standard", "stringent"])
+                .set_default("standard"),
+            ]
+        )
+    ]
